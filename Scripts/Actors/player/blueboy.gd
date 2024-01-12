ea333ea45a8ba4e38053fdf93f83c1fe0fe3f578
FreelyTian <osk9ehdms01@gmail.com>
2024-01-12 17:44:26 -0300

    Bugfixes no script de movimento do player

--- Scripts/Actors/player/blueboy.gd
+++ Scripts/Actors/player/blueboy.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var animtree = $mesh/AnimationPlayer/AnimationTree
 @export var movedata : PlayerMovementData
 var _direction = Vector2.ZERO
+var _last_direction = Vector2.ZERO
 
 func _ready():
 	animtree.active = true
@@ -13,15 +14,18 @@ func _physics_process(delta):
 	move_and_slide()
 	
 func move():
+	var _last_direction_bkp = _last_direction
 	_direction = Input.get_vector("a", "d", "w", "s").normalized()
+	_last_direction = _direction if _direction != Vector2.ZERO else _last_direction_bkp
 	if _direction != Vector2.ZERO:
 		set_walking(true)
 		update_blend_pos()
+		#animtree.active = true
 		do_vel(movedata.accel)
 	else:
 		set_walking(false)
-		update_blend_pos()
 		do_vel(movedata.fric)
+	
 		
 func do_vel(factor):
 	velocity.x = lerp(velocity.x, _direction.x * movedata.SPEED, factor)
@@ -32,7 +36,7 @@ func set_walking(value):
 	animtree.set("parameters/conditions/idle", not value)
 	
 func update_blend_pos():
-	animtree["parameters/walk/blend_position"] = _direction
 	animtree["parameters/idle/blend_position"] = _direction
+	animtree["parameters/walk/blend_position"] = _direction
 	
-	
+
